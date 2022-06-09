5b92dd98f4f159809059ac52c254818fb16a7161
vigneaud-khalid <vigneaud40@yahoo.fr>
2022-06-09 18:17:37 +0200

    dependencies added

---
+++ app/src/main/java/com/cleanup/todoc/viewmodel/TaskViewModel.java
@@ -0,0 +1,11 @@
+package com.cleanup.todoc.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * Created by ordinateur _ Khalid _  on 09/06/2022.
+ */
+public class TaskViewModel extends ViewModel {
+
+
+}
